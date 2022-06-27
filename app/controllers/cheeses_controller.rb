519258718124d76f507ecdceda62380c7d1ca15d
Kevin Nielsen <knielsen24@gmail.com>
2022-06-27 10:01:54 -0700

    commit tree 839b68e7e87b643b983a5ea2e389467238b3fb70

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,15 +1,20 @@
 class CheesesController < ApplicationController
 
   # GET /cheeses
-  def index
-    cheeses = Cheese.all
-    render json: cheeses
-  end
+	def index
+		cheeses = Cheese.all
+		render json: cheeses, except: [:created_at, :updated_at], methods: [:summary]
+	end
 
-  # GET /cheeses/:id
-  def show
-    cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
-  end
+	# GET /cheeses/:id
+	def show
+		cheese = Cheese.find_by(id: params[:id])
+		if cheese
+			render json: cheese
+		else
+			render json: { error: "Cheese not found" }, status: :not_found
+		end
+
+	end
 
 end
